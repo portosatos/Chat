9181689a5a0ae8628d96dea868c7d12c28c402d2
portosatos <93531068+portosatos@users.noreply.github.com>
2024-12-25 15:06:37 +0000

    asd

--- backend/app.py
+++ backend/app.py
@@ -117,3 +117,6 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Создаст базы данных
     socketio.run(app, host='0.0.0.0', port=5000)  # Запуск на всех интерфейсах
+
+
+#fghjhghjhg
